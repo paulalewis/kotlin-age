562b2072ffc0d26c2ffe8a1ca3ff189abda1d062
Paul Lewis <paularthurlewis@gmail.com>
2014-03-10 10:36:59 -0700

    added support for yaml test cases

--- src/com/castlefrog/agl/DomainTest.java
+++ src/com/castlefrog/agl/DomainTest.java
@@ -1,12 +1,10 @@
 package com.castlefrog.agl;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,12 +17,20 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * This class is used to run tests between agents on simulators.
  */
 public final class DomainTest {
-    public static final String TEST_ROOT_NAME = "domainTest";
+    private static final String ROOT_ELEMENT = "domainTest";
+    private static final String N_SIMULATIONS_ELEMENT = "nSimulations";
+    private static final String DOMAIN_ELEMENT = "domain";
+
+    private List<Agent> agents = new ArrayList<>();
+    private Simulator<?, ?> domain;
+    private int nSimulations;
+
     /**
      * @param args
      *      test_filename - path to the xml test file
@@ -35,65 +41,24 @@ public final class DomainTest {
             registerSimulators();
             registerAgents();
             try {
-                List<Agent> agents = new ArrayList<>();
-                File xmlFile = new File(args[0]);
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                Document doc = dBuilder.parse(xmlFile);
-                doc.getDocumentElement().normalize();
-
-                if (!doc.getDocumentElement().getNodeName().equals(TEST_ROOT_NAME)) {
-                    throw new IllegalArgumentException("Root tag should be '" + TEST_ROOT_NAME + "'");
-                }
-                int nSimulations = Integer.parseInt(getTagValue("nSimulations", doc.getDocumentElement()));
-                NodeList nList = doc.getElementsByTagName("world");
-                Element element = (Element) nList.item(0);
-                String simulatorName = getTagValue("name", element);
-                NodeList paramList = element.getElementsByTagName("params");
-                List<String> simulatorParams = new ArrayList<>();
-                if (paramList.getLength() != 0) {
-                    NodeList paramNodes = ((Element) paramList.item(0)).getElementsByTagName("param");
-                    for (int j = 0; j < paramNodes.getLength(); j += 1) {
-                        Node paramNode = paramNodes.item(j);
-                        if (paramNode.getNodeType() == Node.ELEMENT_NODE) {
-                            simulatorParams.add(paramNode.getTextContent());
-                        }
-                    }
-                }
-                Simulator<?, ?> world = Simulators.getSimulator(simulatorName, simulatorParams);
-
-                NodeList agentList = doc.getElementsByTagName("agents");
-                NodeList agentNodes = ((Element) agentList.item(0)).getElementsByTagName("agent");
-                for (int i = 0; i < agentNodes.getLength(); i += 1) {
-                    Element agentNode = (Element) agentNodes.item(i);
-                    if (agentNode.getNodeType() == Node.ELEMENT_NODE) {
-                        String name = getTagValue("name", agentNode);
-                        paramList = agentNode.getElementsByTagName("params");
-                        List<String> params = new ArrayList<>();
-                        if (paramList.getLength() != 0) {
-                            NodeList paramNodes = ((Element) paramList.item(0)).getElementsByTagName("param");
-                            for (int j = 0; j < paramNodes.getLength(); j += 1) {
-                                Node paramNode = paramNodes.item(j);
-                                if (paramNode.getNodeType() == Node.ELEMENT_NODE) {
-                                    params.add(paramNode.getTextContent());
-                                }
-                            }
-                        }
-                        agents.add(Agents.getAgent(name, params));
-                    }
+                File inputFile = new File(args[0]);
+                if (inputFile.getName().contains(".yaml")) {
+                    readYamlFile(inputFile);
+                } else if (inputFile.getName().contains(".xml")) {
+                    readXmlFile(inputFile);
+                } else {
+                    throw new IllegalArgumentException("Invalid file type: " + args[0]);
                 }
 
+                List<Simulator<?, ?>> simulators = new ArrayList<>();
                 List<double[]> rewardsData = new ArrayList<>();
                 List<double[]> avgMoveTimeData = new ArrayList<>();
                 for (int i = 0; i < agents.size(); i += 1) {
+                    simulators.add(domain.copy());
                     rewardsData.add(new double[nSimulations]);
                     avgMoveTimeData.add(new double[nSimulations]);
                 }
-                List<Simulator<?, ?>> simulators = new ArrayList<>();
-                for (int i = 0; i < agents.size(); i += 1) {
-                    simulators.add(world.copy());
-                }
-                Arbiter<?, ?> arbiter = new Arbiter(world.getState(), world, simulators, agents);
+                Arbiter<?, ?> arbiter = new Arbiter(domain.getState(), domain, simulators, agents);
                 for (int i = 0; i < nSimulations; i += 1) {
                     arbiter.reset();
                     try {
@@ -125,7 +90,7 @@ public final class DomainTest {
                 output.append("#");
                 output.append(nSimulations);
                 output.append(" - ");
-                output.append(world.getClass().getSimpleName());
+                output.append(domain.getClass().getSimpleName());
                 output.append("\n");
 
                 DecimalFormat df = new DecimalFormat("#.###");
@@ -162,8 +127,86 @@ public final class DomainTest {
         }
     }
 
+    private void readXmlFile(File file) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
+        doc.getDocumentElement().normalize();
+
+        if (!doc.getDocumentElement().getNodeName().equals(ROOT_ELEMENT)) {
+            throw new IllegalArgumentException("Root tag should be '" + ROOT_ELEMENT + "'");
+        }
+        nSimulations = Integer.parseInt(getTagValue(N_SIMULATIONS_ELEMENT, doc.getDocumentElement()));
+        NodeList nList = doc.getElementsByTagName(DOMAIN_ELEMENT);
+        Element element = (Element) nList.item(0);
+        String simulatorName = getTagValue("name", element);
+        NodeList paramList = element.getElementsByTagName("params");
+        List<String> simulatorParams = new ArrayList<>();
+        if (paramList.getLength() != 0) {
+            NodeList paramNodes = ((Element) paramList.item(0)).getElementsByTagName("param");
+            for (int j = 0; j < paramNodes.getLength(); j += 1) {
+                Node paramNode = paramNodes.item(j);
+                if (paramNode.getNodeType() == Node.ELEMENT_NODE) {
+                    simulatorParams.add(paramNode.getTextContent());
+                }
+            }
+        }
+
+        domain = Simulators.getSimulator(simulatorName, simulatorParams);
+
+        NodeList agentList = doc.getElementsByTagName("agents");
+        NodeList agentNodes = ((Element) agentList.item(0)).getElementsByTagName("agent");
+        for (int i = 0; i < agentNodes.getLength(); i += 1) {
+            Element agentNode = (Element) agentNodes.item(i);
+            if (agentNode.getNodeType() == Node.ELEMENT_NODE) {
+                String name = getTagValue("name", agentNode);
+                paramList = agentNode.getElementsByTagName("params");
+                List<String> params = new ArrayList<>();
+                if (paramList.getLength() != 0) {
+                    NodeList paramNodes = ((Element) paramList.item(0)).getElementsByTagName("param");
+                    for (int j = 0; j < paramNodes.getLength(); j += 1) {
+                        Node paramNode = paramNodes.item(j);
+                        if (paramNode.getNodeType() == Node.ELEMENT_NODE) {
+                            params.add(paramNode.getTextContent());
+                        }
+                    }
+                }
+                agents.add(Agents.getAgent(name, params));
+            }
+        }
+    }
+
     private String getTagValue(String tagName, Element element) {
         return element.getElementsByTagName(tagName).item(0).getChildNodes().item(0).getNodeValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void readYamlFile(File file) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        Map<String, Object> root = (Map<String, Object>) yaml.load(new FileInputStream(file));
+        // number of simulations
+        nSimulations = (int) root.get(N_SIMULATIONS_ELEMENT);
+        // domain
+        Map<String, Object> domainSimulator = (Map<String, Object>) root.get(DOMAIN_ELEMENT);
+        String domainName = (String)domainSimulator.get("name");
+        List<String> domainParams = getYamlStringList((List<Object>) domainSimulator.get("params"));
+        domain = Simulators.getSimulator(domainName, domainParams);
+        // agents
+        List<Map<String, Object>> agentList = (List<Map<String, Object>>)root.get("agents");
+        for (Map<String, Object> agentData: agentList) {
+            List<String> params = getYamlStringList((List<Object>) agentData.get("params"));
+            agents.add(Agents.getAgent((String)agentData.get("name"), params));
+        }
+    }
+
+    private List<String> getYamlStringList(List<Object> list) {
+        List<String> output = new ArrayList<>();
+        if (list != null) {
+            for (Object item: list) {
+                output.add(item.toString());
+            }
+        }
+        return output;
     }
 
     private void recordResults(String filename, String results) {
