380da66d4e7e218e60148ffaae1ee8c4e300a23f
Paul Lewis <paularthurlewis@gmail.com>
2014-03-11 15:14:39 -0700

    connect4 simulator provider now accepts turn type as param

--- src/com/castlefrog/agl/domains/connect4/Connect4SimulatorProvider.java
+++ src/com/castlefrog/agl/domains/connect4/Connect4SimulatorProvider.java
@@ -1,23 +1,49 @@
 package com.castlefrog.agl.domains.connect4;
 
 import java.util.List;
+import java.util.Map;
 
 import com.castlefrog.agl.SimulatorProvider;
 import com.castlefrog.agl.Simulators;
+import com.castlefrog.agl.TurnType;
 
 public final class Connect4SimulatorProvider implements SimulatorProvider {
+    private static final String TURN_TYPE_ARG_NAME = "turn";
+
+    private static Connect4SimulatorProvider instance_;
+
     static {
         Simulators.registerProvider("connect4", Connect4SimulatorProvider.getInstance());
     }
 
-    private Connect4SimulatorProvider() {
-    }
+    private Connect4SimulatorProvider() {}
 
     public static Connect4SimulatorProvider getInstance() {
-        return new Connect4SimulatorProvider();
+        if (instance_ == null) {
+            instance_ = new Connect4SimulatorProvider();
+        }
+        return instance_;
     }
 
     public Connect4Simulator newSimulator(List<String> params) {
-        return Connect4Simulator.create(params);
+        TurnType turnType = TurnType.SEQUENTIAL;
+        for (int i = 0; i < params.size(); i += 1) {
+            switch (params.get(i).toLowerCase()) {
+                case TURN_TYPE_ARG_NAME:
+                    break;
+            }
+            if (params.get(i).equalsIgnoreCase(TURN_TYPE_ARG_NAME)) {
+                if (params.size() > i + 1) {
+                    turnType = TurnType.valueOf(params.get(i + 1).toUpperCase());
+                }
+            }
+        }
+        return Connect4Simulator.create(turnType);
     }
+
+    //public Connect4Simulator newSimulator(Map<String, String> params) {
+    //    String turnTypeArg = params.get(TURN_TYPE_ARG_NAME);
+    //    TurnType turnType = (turnTypeArg == null) ? TurnType.SEQUENTIAL : TurnType.valueOf(turnTypeArg.toUpperCase());
+    //    return Connect4Simulator.create(turnType);
+    //}
 }
