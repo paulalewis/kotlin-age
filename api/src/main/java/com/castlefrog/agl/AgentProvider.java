dd8a5b63338aeb239a26965bce160b592a0ce93f
Paul Lewis <paularthurlewis@gmail.com>
2016-02-28 10:28:23 -0800

    delete providers

--- api/src/main/java/com/castlefrog/agl/AgentProvider.java
+++
@@ -1,7 +0,0 @@
-package com.castlefrog.agl;
-
-import java.util.List;
-
-public interface AgentProvider {
-    Agent newAgent(List<String> params);
-}
--- api/src/main/java/com/castlefrog/agl/SimulatorProvider.java
+++
@@ -1,7 +0,0 @@
-package com.castlefrog.agl;
-
-import java.util.List;
-
-public interface SimulatorProvider {
-    Simulator<?, ?> newSimulator(List<String> params);
-}
