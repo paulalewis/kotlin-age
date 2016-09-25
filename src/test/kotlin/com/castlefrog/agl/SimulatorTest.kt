db11091f38aeb5aebdc0b844f1ae26310509b9dd
Paul Lewis <paularthurlewis@gmail.com>
2016-09-24 17:04:10 -0700

    test simulator without hex domain

--- src/test/kotlin/com/castlefrog/agl/SimulatorTest.kt
+++ src/test/kotlin/com/castlefrog/agl/SimulatorTest.kt
@@ -1,7 +1,5 @@
 package com.castlefrog.agl
 
-import com.castlefrog.agl.domains.hex.HexAction
-import com.castlefrog.agl.domains.hex.HexSimulator
 import com.google.common.truth.Truth
 import org.junit.Test
 
@@ -9,24 +7,25 @@ class SimulatorTest {
 
     @Test
     fun nAgents() {
-        val simulator = HexSimulator.create(5, true)
+        val simulator = TestSimulator(state = TestState(),
+                legalActions = arrayListOf(arrayListOf(), arrayListOf()),
+                rewards = intArrayOf(0, 0))
         Truth.assertThat(simulator.nAgents).isEqualTo(2)
     }
 
     @Test
     fun isTerminalStateFalse() {
-        val simulator = HexSimulator.create(5, true)
+        val simulator = TestSimulator(state = TestState(),
+                legalActions = arrayListOf(arrayListOf(TestAction()), arrayListOf()),
+                rewards = intArrayOf(0, 0))
         Truth.assertThat(simulator.isTerminalState).isFalse()
     }
 
     @Test
     fun isTerminalStateTrue() {
-        val simulator = HexSimulator.create(3, false)
-        simulator.stateTransition(arrayListOf(HexAction.valueOf(0, 0), null))
-        simulator.stateTransition(arrayListOf(null, HexAction.valueOf(0, 1)))
-        simulator.stateTransition(arrayListOf(HexAction.valueOf(1, 1), null))
-        simulator.stateTransition(arrayListOf(null, HexAction.valueOf(0, 2)))
-        simulator.stateTransition(arrayListOf(HexAction.valueOf(2, 2), null))
+        val simulator = TestSimulator(state = TestState(),
+                legalActions = arrayListOf(arrayListOf(), arrayListOf()),
+                rewards = intArrayOf(0, 0))
         Truth.assertThat(simulator.isTerminalState).isTrue()
     }
 
