b0025f8abfae864778f479fe827d68f5abfd1d33
Paul Lewis <paularthurlewis@gmail.com>
2017-02-18 07:18:58 -0800

    remove adversarial n players

--- src/main/kotlin/com/castlefrog/agl/Util.kt
+++ src/main/kotlin/com/castlefrog/agl/Util.kt
@@ -1,6 +1,5 @@
 package com.castlefrog.agl
 
-val ADVERSARIAL_N_PLAYERS = 2
 val ADVERSARIAL_REWARDS_NEUTRAL = intArrayOf(0, 0)
 val ADVERSARIAL_REWARDS_BLACK_WINS = intArrayOf(1, -1)
 val ADVERSARIAL_REWARDS_WHITE_WINS = intArrayOf(-1, 1)
