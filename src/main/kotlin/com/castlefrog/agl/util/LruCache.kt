cea32a15b2fc5e14b42670b62cce423fe91e37c3
Paul Lewis <paularthurlewis@gmail.com>
2017-12-04 11:59:10 -0800

    update kotlin

--- src/main/kotlin/com/castlefrog/agl/util/LruCache.kt
+++ src/main/kotlin/com/castlefrog/agl/util/LruCache.kt
@@ -4,7 +4,5 @@ package com.castlefrog.agl.util
  * Implementation of least recently used cache.
  */
 class LruCache<K, V>(private val cacheSize: Int) : LinkedHashMap<K, V>(16, 0.75f, true) {
-    override fun removeEldestEntry(eldest: Map.Entry<K, V>): Boolean {
-        return size >= cacheSize
-    }
+    override fun removeEldestEntry(eldest: Map.Entry<K, V>): Boolean = size >= cacheSize
 }
